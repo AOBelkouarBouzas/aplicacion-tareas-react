e8d276b81e3d97da52b61fee8b867c9013dc0419
AOBelkouarBouzas <125385570+AOBelkouarBouzas@users.noreply.github.com>
2024-02-20 19:02:03 -0300

    add style to tarea completada

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -2,11 +2,15 @@ import React from "react";
 import "../hojas-de-estilo/Tarea.css";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-export const Tarea = ({ texto }) => {
+export const Tarea = ({ texto, completada }) => {
   return (
-    <div className="tarea-contenedor">
+    <div
+      className={
+        completada ? "tarea-contenedor completada" : "tarea-contenedor"
+      }
+    >
       <div className="tarea-texto">{texto}</div>
-      <div className="tarea-icono">
+      <div className="tarea-contenedor-iconos">
         <AiOutlineCloseCircle className="tarea-icono" />
       </div>
     </div>
